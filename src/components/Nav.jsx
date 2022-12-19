ea9e09d32b39fdd1343966675bd686fe3b8de73d
Javascript <javascript@Javascripts-MacBook-Pro.local>
2022-12-19 23:31:41 +0100

    Home

---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+const Nav = () => {
+  return (
+    <nav className="flex sm:justify-center space-x-4 bg-black">
+      <div className="py-2">
+        <img width="30px" src="./images/apple-icon.png" />
+      </div>
+    </nav>
+  );
+};
+export default Nav;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Nav from "../components/Nav";
+import Main from "../components/Main";
+const Home = () => {
+  return (
+    <>
+      <Nav />
+      <Main />
+    </>
+  );
+};
+
+export default Home;
