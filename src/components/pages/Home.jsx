cf96ac058a7c83fc495e02425e9c589ac37e42a8
inext.dev <33235992+inextdeve@users.noreply.github.com>
2022-12-19 18:16:06 +0000

    Initialize the project

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return <>
+    <nav className="flex sm:justify-center space-x-4 bg-black">
+        <div className="py-2">
+            <img width="30px" src="./images/apple-icon.png" />
+        </div>
+        </nav>
+    </>
+}
+
+export default Home;
