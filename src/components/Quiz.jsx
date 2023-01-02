8971e3ddbe1c5fe5fba6663d49c8230bd00a5bc5
Javascript <javascript@Javascripts-MacBook-Pro.local>
2023-01-02 07:08:50 +0100

    last mod

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,58 +1,65 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { GiftContext } from "../context/context";
 
 const Quiz = () => {
+  const [i, setI] = useState(0);
 
-    const [i, setI] = useState(0);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const { setQuestionPassed } = useContext(GiftContext);
 
+  const quizQuestion = [
+    {
+      question: "السؤال 1 من 4: هل انت ذكر ام انثى ؟",
+      answers: ["ذكر", "أنثى"],
+    },
+    {
+      question: "السؤال 2 من 4: كم عمرك ؟",
+      answers: ["18-29", "30-39", "40-49", "+50"],
+    },
+    {
+      question: "ما هو تقييمك لخدمات آبل ؟",
+      answers: ["جيد جدا", "لا يصدق", "نعم", "ليس جيد جدا"],
+    },
+    {
+      question: "السؤال 4 من 4: ما هو الهاتف الذكي الذي تستخدمه؟",
+      answers: ["Android", "Apple"],
+    },
+  ];
 
-    const quizQuestion = [
-        {
-            question: "السؤال 1 من 4: هل انت ذكر ام انثى ؟",
-            answers: ["ذكر", "أنثى"]
-        },
-        {
-            question: "السؤال 2 من 4: كم عمرك ؟",
-            answers: ["18-29", "30-39", "40-49", "+50"]
-        },
-        {
-            question: "ما هو تقييمك لخدمات آبل ؟",
-            answers: ["جيد جدا", "لا يصدق", "نعم", "ليس جيد جدا"]
-        },
-        {
-            question: "السؤال 4 من 4: ما هو الهاتف الذكي الذي تستخدمه؟",
-            answers: ["Android", "Apple"]
-        }
-    ];
+  const [animation, setAnimation] = useState("fadeIn");
 
-    const [animation, setAnimation] = useState("fadeIn");
+  const handleClick = () => {
+    setAnimation("fadeOut");
 
-    const handleClick = () => {
-        setAnimation("fadeOut");
-
-        setTimeout(() => {
-            setI(prev => prev+=1);
-            setAnimation("fadeIn");
-        }, 1100)
-        if (i === quizQuestion.length - 1) {
-            setI(0)
-            navigate("/randomgift")
-        }
+    setTimeout(() => {
+      setI((prev) => (prev += 1));
+      setAnimation("fadeIn");
+    }, 1100);
+    if (i === quizQuestion.length - 1) {
+      setI(0);
+      setQuestionPassed(true);
+      navigate("/randomgift");
     }
+  };
 
-    
-
-    return <div>
-        <div className="question">
-            {quizQuestion[i].question}
-        </div>
-        <div className="grid my-4">
-            {quizQuestion[i].answers.map((answer, index) => (<button key={index} onClick={handleClick} className={`${animation} rounded p-3 my-3 block btn btn__primary`}>{answer}</button>))}
-        </div>
+  return (
+    <div>
+      <div className="question">{quizQuestion[i].question}</div>
+      <div className="grid my-4">
+        {quizQuestion[i].answers.map((answer, index) => (
+          <button
+            key={index}
+            onClick={handleClick}
+            className={`${animation} rounded p-3 my-3 block btn btn__primary`}
+          >
+            {answer}
+          </button>
+        ))}
+      </div>
     </div>
-}
+  );
+};
 
-export default Quiz
+export default Quiz;
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,34 +1,48 @@
 import { createContext, useState } from "react";
 
-export const GiftContext = createContext()
+export const GiftContext = createContext();
 
-const GiftProvider = ({children}) => {
+const GiftProvider = ({ children }) => {
+  let init = JSON.parse(localStorage.getItem("player") || "{}");
 
-    const [giftClick, setGiftClick] = useState(0);
-    const [isWin, setIsWin] = useState(false);
-    const [winnerName, setWinnerName] = useState("");
-    const [passed, setPassed] = useState(false);
+  if (init?.expiry < new Date().getTime()) {
+    localStorage.removeItem("player");
+    init = {};
+  }
 
-    const incClick = () => {
-        setGiftClick(prev => prev + 1 )
-    }
+  const [giftClick, setGiftClick] = useState(0);
+  const [isWin, setIsWin] = useState(false);
+  const [winnerName, setWinnerName] = useState(init?.name || "");
+  const [passed, setPassed] = useState(false);
+  const [questionPassed, setQuestionPassed] = useState(false);
+  const [giftPassed, setGiftPassed] = useState(init?.giftPassed || false);
 
-    const winner = () => {
-        setIsWin(true)
-    }
+  const incClick = () => {
+    setGiftClick((prev) => prev + 1);
+  };
 
-    const giftState = {
-        giftClick,
-        incClick,
-        isWin,
-        winner,
-        winnerName,
-        setWinnerName,
-        passed,
-        setPassed
-    }
+  const winner = () => {
+    setIsWin(true);
+  };
 
-    return <GiftContext.Provider value={giftState}>{children}</GiftContext.Provider>
-}
+  const giftState = {
+    giftClick,
+    incClick,
+    isWin,
+    winner,
+    winnerName,
+    setWinnerName,
+    passed,
+    setPassed,
+    questionPassed,
+    setQuestionPassed,
+    giftPassed,
+    setGiftPassed,
+  };
+
+  return (
+    <GiftContext.Provider value={giftState}>{children}</GiftContext.Provider>
+  );
+};
 
 export default GiftProvider;
